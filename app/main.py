e831bb24c3adab4202d23ea5b488b1896b203f05
Linh Nguyen <linh.nguyen361991@gmail.com>
2024-01-31 17:32:25 +0200

    add unit test

--- app/main.py
+++ app/main.py
@@ -7,10 +7,10 @@ app = FastAPI(title="Delivery Fee API")
 
 @app.post("/calculate_delivery_fee", response_model=DeliveryResponse)
 def calculate_delivery_fee(request: DeliveryRequest):
-    # Create a DeliveryCalculator object
+    """
+    Calculate the delivery fee based on the request JSON
+    """
     calculator = DeliveryCalculator(request)
-
-    # Calculate the delivery fee
     delivery_fee = calculator.total_fee()
 
     return DeliveryResponse(delivery_fee=delivery_fee)
